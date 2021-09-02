a0af0e202081ee944917e743c65927dec50681a6
vikram7102 <bhosalevikram7102@gmail.com>
2021-09-02 19:29:13 +0530

    js basic

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hey this is my 1 st js code")
+console.log(8753787774)
+console.log({me:"vikram"})
+console.log(true)
+console.log(false)
+console.log(true,7375773,{place:"jejuri"})
+console.warn("this is warning");
+console.error("this is ann error")
