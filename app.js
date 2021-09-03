fdfab6560750d3a89c7a56a1b34eea307746ed6a
vikram7102 <bhosalevikram7102@gmail.com>
2021-09-03 20:48:54 +0530

    conditional statements

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-//logical operators
-const a=2
-const b=3
-const c=4
-console.log(c>a && c<b)
-// console.log('a==b',a==b)
-// console.log('a!=b',a!=b)
-// console.log('truth table for and .or')
-// console.log('false && false',false && false)
-// console.log('true && false',true && false)
-// console.log('true && true',true && true)
-// console.log('false && true',false && true)
-
-// console.log('false || false',false || false)
-// console.log('true || false',true || false)
-// console.log('true || true',true || true)
-// console.log('false || true',false || true)
+//conditionals statements
+// even or odd
+const a=7
+if(typeof 'number'){
+    
+    if(a%2===0){
+        console.log('the no is even')
+    }
+    
+    else{
+        console.log('the no is odd')
+    }
+}
+else{
+    console.error('not a no')
+}
 
