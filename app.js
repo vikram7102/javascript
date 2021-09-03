10b6a60f457dad28ea007a6a9da3701e8166c6ba
vikram7102 <bhosalevikram7102@gmail.com>
2021-09-03 20:19:01 +0530

    logical operators

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+//logical operators
+const a=2
+const b=3
+const c=4
+console.log(c>a && c<b)
+// console.log('a==b',a==b)
+// console.log('a!=b',a!=b)
+// console.log('truth table for and .or')
+// console.log('false && false',false && false)
+// console.log('true && false',true && false)
+// console.log('true && true',true && true)
+// console.log('false && true',false && true)
 
-//comparison operators
-const a=21
-const b =7
-console.log('a>b',a>b)
-console.log(a<b)
-console.log(a==b)
-
-const c= 2
-const d= '2'
-console.log(c==d)
-console.log(c===d)
-
-const f= 'vikram'
-const h= 'VIKRAM'
-console.log(f==h)
-
+// console.log('false || false',false || false)
+// console.log('true || false',true || false)
+// console.log('true || true',true || true)
+// console.log('false || true',false || true)
 
