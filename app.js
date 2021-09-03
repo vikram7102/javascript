eead10c8aa72e471531b69d77b764331d12667c6
vikram7102 <bhosalevikram7102@gmail.com>
2021-09-03 20:04:37 +0530

    comparison operator

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 
-//assignment operator
-let name ="snehal"
-let a=9
-a+=name
-console.log(a)
-console.log(typeof a)
+//comparison operators
+const a=21
+const b =7
+console.log('a>b',a>b)
+console.log(a<b)
+console.log(a==b)
+
+const c= 2
+const d= '2'
+console.log(c==d)
+console.log(c===d)
+
+const f= 'vikram'
+const h= 'VIKRAM'
+console.log(f==h)
+
 
