617a4c0a52e9d64a12d603ff7ee258bb33b42563
vikram7102 <bhosalevikram7102@gmail.com>
2021-09-03 19:49:11 +0530

    assignment operator

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
 
-//operators in js
-//arithmetic operators
-let age = 19
-age =25
-console.log(age)
-console.log('---------------arithmetic operators----------------')
-const a=2
-const b=7
-let c=0
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a**b)
-console.log(a%b)
-console.log('---------------assignment operators----------------')
+//assignment operator
+let name ="snehal"
+let a=9
+a+=name
+console.log(a)
+console.log(typeof a)
 
-
-c+=1 //increament operator
-console.log(c)
-c++
-console.log(c)
-let g=5
-g*=5 // g=g*5
-console.log(g)
-g**=3
-console.log(g)
