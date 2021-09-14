4eeb239327815431168b668ea5636fce5e74fabe
vikram7102 <bhosalevikram7102@gmail.com>
2021-09-14 17:40:09 +0530

    dom with harry

--- app.js
+++ app.js
@@ -1,72 +1,26 @@
+let elem=document.getElementById('click');
+console.log(elem)
 
-// es6 function
-let add=(x,y)=>x+y;
-console.log(add(2,3));
+let elemclass =document.getElementsByClassName('container')
+console.log(elemclass)
+// elemclass[0].style.background='yellow';
+//console.log()
+elemclass[0].classList.add("bg-primary")
+elemclass[0].classList.add("text-success")
+elemclass[0].classList.remove("text-success")
+// elemclass[0].innerHTML
+// elemclass[0].innerText
+console.log(elem.innerHTML)
+console.log(elem.innerText)
+console.log(elemclass[0].innerHTML)
+console.log(elemclass[0].innerText)
 
-let sub =(a,b)=>{
-    return a-b;
-}
-console.log(sub(5,3));
+let tn=document.getElementsByTagName('button')
+console.log(tn)
 
+//selecting using query
 
-var arr = [1,2,3,4];
-console.log(arr[2]);
-
-var [one, two, three, four, five] = arr; //ES6 Destructuring
-console.log(three);
-console.log(four);
-console.log(five);
-
-
-function getscores() {
-    return [90,100];
-}
-
-let [x,y,z] = getscores();
-console.log(x);
-console.log(y);
-console.log(z);
-
-
-const obj = {
-    fullname: "Nikhil Agarwal",
-    email: "nikhilagarwaliitkgp@gmail.com",
-    age: 20
-}
-
-console.log(obj.email)
-
-let {dum, age, email, fullname} = obj;
-console.log(fullname)
-console.log(dum)
-
-
-let person = {
-    firstname: "Nikhil",
-    lastname: "Agarwal",
-    age1: 20,
-    middlename: "DNA"
-}
-let {age1, firstname, lastname, middlename="" } = person;
-console.log(age1);
-console.log(firstname);
-console.log(lastname);
-console.log(middlename);
-
-const counter={
-    count:1,
-    vikram: function(){
-        return ++(this.count);
-    },
-    key:this,
-    bhosale:() =>{
-        return --(this.count)
-    } 
-}
-console.log(counter)
-console.log(counter.vikram)
-console.log(counter.bhosale)
-console.log(counter)
-
-//
-
+sel=document.querySelector('.container')
+console.log(sel)
+sel=document.querySelectorAll('.container')
+console.log(sel)
