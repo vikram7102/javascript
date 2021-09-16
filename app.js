ad7d7b6ca5526095dfa287cb9064d6c47fc51507
vikram7102 <bhosalevikram7102@gmail.com>
2021-09-16 15:17:00 +0530

    coh js event backtick localstorage

--- app.js
+++ app.js
@@ -24,3 +24,55 @@ sel=document.querySelector('.container')
 console.log(sel)
 sel=document.querySelectorAll('.container')
 console.log(sel)
+
+//events in js
+
+// function clicked(){
+//     console.log('the button was clicked')
+// }
+// window.onload=function(){
+//     console.log('the document was loaded')
+// }
+// firstcontainer.addEventListener('click',function(){
+//     document.querySelectorAll('.container')[1].innerHTML="<b> we have clicked</b>"
+//     console.log('click on container')
+// })
+// firstcontainer.addEventListener('mouseover',function(){
+//     console.log('mouse  on container')
+// })
+// firstcontainer.addEventListener('mouseout',function(){
+//     console.log('mouse out  of container')
+// })
+
+// settime and setinterval
+// logkaro=()=> {
+//     console.log('im your log')
+// }
+// setTimeout(logkaro,5000); //after 5 sec im log will get 
+// clr=setInterval(logkaro,2000); 
+// use clearInterval and clearTimeout to cancal the setTimeout or setTimeout
+// clearInterval(clr);
+
+
+// js local storage
+
+
+// localStorage.setItem('name','vikram');
+// localStorage
+// localStorage.getItem('name')
+// localStorage.removeItem('Name') // removes specific item from the local storage
+// localStorage.clear()// clears all the items of the local storage
+
+//json javascript object notation
+
+// obj={Name:'vikram',length:1}
+// jso=JSON.stringify(obj);  // converts object into string
+// console.log(typeof(jso)) // string 
+// console.log(jso)
+// parsed=JSON.parse(`{"Name":"vikram","length":1}`) // converts strings into object
+// console.log(parsed)
+
+
+// backtick
+a=34
+console.log(`this is the ${a}`) // prints a value in console.log
